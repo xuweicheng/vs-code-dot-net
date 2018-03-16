c3bcea80e330d764ba1d88ed3ba029b6319065c1
xuweicheng <weicheng.xu@gmail.com>
2018-03-16 09:46:04 -0700

    test vs code and .NET core

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs_code_dot_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
